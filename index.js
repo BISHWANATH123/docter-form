c3381dc99928e53c37a0583bad3cd82d77a5e6e4
Bishwanath kumar mahto <143301935+BISHWANATH123@users.noreply.github.com>
2023-11-26 13:25:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-function student(name,roll_no,clas,section,math,hindi,eng,science){
-    this.name = name;
-    this.roll_no = roll_no;
-    this.class = clas;
-    this.section = section;
-    this.math = math;
-    this.hindi = hindi;
-    this.eng = eng;
-    this.science = science;
-  }
-  
-  
-  
-  var student1 = new student("rahul",20,5,"a",20,30,40,50);
-  
-  
-  
-  console.log("############","report card",
-              student1,
-              
-        `###########`);
+document.addEventListener("DOMContentLoaded",function(){
+    let form = document.getElementById("docter-form");
+    let tableBody = document.getElementById("docter-table-body");
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+
+
+
+        let name = document.getElementById("name").value;
+        let docterId = document.getElementById("docter-id").value;
+        let specialization = document.getElementById("specialization").value;
+        let experience = parseInt( document.getElementById)("experience").value;
+        let email = document.getElementById("email").value;
+        let mobile = document.getElementById("mobile").value;
+
+       let role = "";
+       if(experience > 5){
+        role = "senior";
+       }else if (experience >= 2 && experience <=5){
+        role = "junior";
+       }else{
+        role = "Trainee";
+       }
+
+
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = `
+       <td>${name}</td>
+       <td>${docterId}</td>
+       <td>${specialization}</td>
+       <td>${experience}</td>
+       <td>${email}</td>
+       <td>${mobile}</td>
+       <td>${role}</td>
+       <td><button class="delete-btn">Delete</button></td>
+    
+    `;
+    
+    newRow.querySelector(".delete-btn").addEventListener("click", function(){
+        newRow.remove();
+    });
+    tableBody.appendChild(newRow);
+    form.reset();
+
+    });
+});
